bd74c6262a3785c1feedebd6775f05d78fbcbe66
rishabsingh9 <rishabsinghrs1733@gmail.com>
2023-08-02 13:38:18 +0530

    hello

---
+++ Demo.js
@@ -0,0 +1,15 @@
+//console.dir(document)
+//console.log(document.head)
+//console.log(document.all)
+
+//document.all[10].textContent="Hello"
+
+//console.log(document.getElementById('header-title'))
+var headertitle=document.getElementById('header-title')
+//headertitle.textContent='HELLO'
+//headertitle.innerText='GOODBYE'
+console.log(headertitle)
+console.log(headertitle.textContent)
+console.log(headertitle.innerText)
+//headertitle.innerHTML='<h3> Inner</h3>'
+headertitle.style.borderBottom='solid 3px #000'
