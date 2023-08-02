4a44f8d0696cea66fec218afe8e50d53a69965fc
rishabsingh9 <rishabsinghrs1733@gmail.com>
2023-08-02 17:47:37 +0530

    task qoeryselector added

---
+++ task4.js
@@ -0,0 +1,5 @@
+var second=document.querySelector('.list-group-item:nth-child(2)')
+var third=document.querySelector('.list-group-item:nth-child(3)')
+second.style.backgroundColor="green";
+third.style.display="none"
+console.log(third)
